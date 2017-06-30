8cace699077ae9caba52716fde285347cc54c9f9
nicolasnet <niconetgomez@hotmail.com>
2017-06-30 17:28:22 -0300

    4to cmmit home
    Intentando hacer el ejercicio con el chrome y bloc de notas

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,9 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+var n1 = parseInt(document.getElementById('numeroUno').value);
+var n2 = parseInt(document.getElementById('numeroDos').value);
+var resultado = n1+n2
+alert("la suma es "+resultado)
 }
 
