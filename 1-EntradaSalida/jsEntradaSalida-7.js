7daf2f0c9fe04f017a32c9ee9ac9365e5189015c
nicolasnet <niconetgomez@hotmail.com>
2017-06-30 17:55:24 -0300

    5to commit home
    se sube ej 7   (aparece un error en el chrome con la multiplicación pero hace los 4 cálculos y los muestra correctamente con alert)

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,33 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+var n1 = parseInt(document.getElementById('numeroUno').value);
+var n2 = parseInt(document.getElementById('numeroDos').value);
+var resultado = n1+n2
+alert("la suma es "+resultado)	
 }
 
 function restar()
 {
-	
+var n1 = parseInt(document.getElementById('numeroUno').value);
+var n2 = parseInt(document.getElementById('numeroDos').value);
+var resultado = n1-n2
+alert("la resta es "+resultado)	
 }
 
 function multiplicar()
 { 
-	
+var n1 = parseInt(document.getElementById('numeroUno').value);
+var n2 = parseInt(document.getElementById('numeroDos').value);
+var resultado = n1*n2
+alert("la multiplicación es "+resultado)	
 }
 
 function dividir()
 {
-	
+var n1 = parseInt(document.getElementById('numeroUno').value);
+var n2 = parseInt(document.getElementById('numeroDos').value);
+var resultado = n1/n2
+alert("la divición es "+resultado)	
 }
 
