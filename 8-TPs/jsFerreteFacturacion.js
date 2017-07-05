85b33e9d84be32cd07e97cdfaa651b6fb46a31ae
nicolasnet <niconetgomez@hotmail.com>
2017-07-05 18:23:36 -0300

    1er ejercicio tps. Ferreteria Facturacion. Ofi
    el ejercicio da todo bien, pero igual sale un error en el ultimo alert... no se porque

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,59 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+//de esta manera dividimos los pasos.
+//1ro tomamos los datos literales y los guardamos en variables	
+var PrecioUno=(document.getElementById('PrecioUno').value);
+var PrecioDos=(document.getElementById('PrecioDos').value);
+var PrecioTres=(document.getElementById('PrecioTres').value);
+
+//2do los transformamos en valores
+var PrecioUno=parseInt(PrecioUno);
+var PrecioDos=parseInt(PrecioDos);
+var PrecioTres=parseInt(PrecioTres);
+
+//3ro realizamos la operacion solicitada
+var resultado = PrecioUno+PrecioDos+PrecioTres;
+
+//por ultimo mostramos el resultado segun las caracteristicas pedidas
+alert("la suma es "+resultado)	
 	
 }
 function Promedio () 
 {
-	
+//1ro tomamos los datos literales y los guardamos en variables	
+var PrecioUno=(document.getElementById('PrecioUno').value);
+var PrecioDos=(document.getElementById('PrecioDos').value);
+var PrecioTres=(document.getElementById('PrecioTres').value);
+
+//2do los transformamos en valores
+var PrecioUno=parseInt(PrecioUno);
+var PrecioDos=parseInt(PrecioDos);
+var PrecioTres=parseInt(PrecioTres);
+
+//3ro realizamos la operacion solicitada
+var resultado = (PrecioUno+PrecioDos+PrecioTres)/3;
+
+//por ultimo mostramos el resultado segun las caracteristicas pedidas
+alert("el promedio es "+resultado)		
 }
 function PrecioFinal () 
 {
-	
+//1ro tomamos los datos literales y los guardamos en variables	
+var PrecioUno=(document.getElementById('PrecioUno').value);
+var PrecioDos=(document.getElementById('PrecioDos').value);
+var PrecioTres=(document.getElementById('PrecioTres').value);
+
+//2do los transformamos en valores
+var PrecioUno=parseInt(PrecioUno);
+var PrecioDos=parseInt(PrecioDos);
+var PrecioTres=parseInt(PrecioTres);
+
+//3ro realizamos las operaciones solicitadas y almacenamos resultados en variables
+var PrecioFinaluno = PrecioUno*1.21;
+var PrecioFinaldos = PrecioDos*1.21;
+var PrecioFinaltres = PrecioTres*1.21;
+
+//por ultimo mostramos el resultado segun las caracteristicas pedidas
+alert("Precio Uno Final: $"+PrecioFinaluno+" / Precio Dos Final: $"+PrecioFinaldos+" / Precio Tres Final: $"+PrecioFinaltres);			
 }
