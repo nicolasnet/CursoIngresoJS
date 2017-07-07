1ad974e2933709a4e2a2c9e3c5d820e988fff865
nicolasnet <niconetgomez@hotmail.com>
2017-07-07 17:17:11 -0300

    Cargo ultimo ejercicio de IF
    ejercicio 10, crear nro al azar y poner alert si aprobó o no

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,25 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero=Math.floor((Math.random() * 10) + 1)
+//lo muestro en consola para chequear funcionamiento
+	console.log(numero)
+
+	if(numero>8)
+	{
+		alert("excelente")
+	}
+	else
+	{
+		if(numero>3)
+		 //en el ejercicio pide mayor y menor a 4, yo lo inclui aca xq es aprobado
+			{
+				alert("APROBO")
+			}
+		else
+			{
+				alert("Vamos, la proxima se puede")
+			}	
+	}
 
 }//FIN DE LA FUNCIÓN
