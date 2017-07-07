1c683c0b510523d4be3e47cfa175ad862fe00214
nicolasnet <niconetgomez@hotmail.com>
2017-07-07 17:50:55 -0300

    subo ejercicio 1 de switch. Ofi
    el ejercicio funciona sin problemas

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,7 +2,29 @@ function Mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
+switch(mesDelAño)
+{
+    case "Enero" : //importante las comillas y los dos puntos 
+    {
+        alert("que comiences bien el año!!!.");
+        break;
+    }
+    case "Marzo" :
+    {
+        alert("a clases!!!.");
+        break;
+    }
+    case "Julio" :
+    {
+        alert("se vienen las vacaciones!!!.");
+        break;
+    }
+    case "Diciembre" :
+    {
+        alert("Felices fiestas!!.");
+        break;
+    }
+}
 
 
 
