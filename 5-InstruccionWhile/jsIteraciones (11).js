28fb1c00059e61af40d465603bd4cf83b7f0b500
nicolasnet <niconetgomez@hotmail.com>
2017-07-25 20:30:14 -0300

    modificacion pagina y ejer parcial
    agregue un icono en el apartado de while, este lo prepare para hacer el modelo de parcial. Estos archivos nuevos serian el nro 11 de iteraciones. Del ejercicio de parcial llegue a hacer los puntos del Nivel 1

---
+++ 5-InstruccionWhile/jsIteraciones (11).js
@@ -0,0 +1,57 @@
+function Mostrar()
+{
+/*
+		//Nivel 1 ej. 1.-
+	var base;
+	var ladoDelCuadrado;
+	var perimetro;
+
+
+
+	base=document.getElementById('BaseCuadrado').value;
+
+	ladoDelCuadrado=Math.sqrt(base);
+
+	perimetro=4*ladoDelCuadrado;
+
+
+
+	alert("el perimetro del cuadrado es: "+perimetro);
+*/
+
+/*	
+		//Nivel 1 ej. 2.-
+	var importe;
+	var precioFinal;
+
+	importe=prompt("ingrese importe");
+	precioFinal=importe*1.21;
+
+	alert("precio final del producto: "+precioFinal);
+*/
+
+/*
+		//Nivel 1 ej. 3
+	var LargoTerreno;
+	var AnchoTerreno;
+	var perimetro;
+	var alambreNecesario;
+
+	LargoTerreno=document.getElementById('LargoTerreno').value;
+	LargoTerreno=parseInt(LargoTerreno);
+	AnchoTerreno=document.getElementById('AnchoTerreno').value;
+	AnchoTerreno=parseInt(AnchoTerreno);
+
+	perimetro=(2*(LargoTerreno+AnchoTerreno));
+	alambreNecesario=perimetro*3;
+
+	alert("el alambre necesario son: "+alambreNecesario+" mts.");
+*/
+
+
+
+
+
+
+
+}//FIN DE LA FUNCIÓN
