483abc8871c035c2cebf261b11a1a0e356d8a020
nicolasnet <niconetgomez@hotmail.com>
2017-07-26 16:45:29 -0300

    hechos Nivel 2 del parcial
    ejercicio 4 y 5 del nivel 2 (if y switch) funcionando, ambos están comentados para que no se mezcle ningún código de los distintos ejercicios.

--- 5-InstruccionWhile/jsIteraciones (11).js
+++ 5-InstruccionWhile/jsIteraciones (11).js
@@ -47,7 +47,63 @@ function Mostrar()
 
 	alert("el alambre necesario son: "+alambreNecesario+" mts.");
 */
+	
+/*
+		//Nivel 2 ej. 4
+	var numeroUno;
+	var numeroDos;
+	var multiplicacion;
+	var resta;
+	var suma,
 
+	numeroUno=prompt("ingrese 1er numero");
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=prompt("ingrese 2do numero");
+	numeroDos=parseInt(numeroDos);
+
+	if(numeroUno==numeroDos)
+	{
+		multiplicacion=numeroUno*numeroDos;
+		document.write("los numeros son iguales, multiplicacion= "+multiplicacion);
+	}
+	else
+	{
+		if(numeroUno>numeroDos)
+		{
+			resta=numeroUno-numeroDos;
+			document.write("el 1ro es mayor, resta= "+resta);
+		}
+		else
+		{
+			suma=numeroUno+numeroDos;
+			document.write("el 2do es mayor, suma= "+suma);
+		}
+	}
+*/
+	
+/*
+		//Nivel 2 ej. 5
+	var diaDeSemana
+
+	diaDeSemana=prompt("ingresar dia de Semana");
+
+	switch(diaDeSemana)
+	{
+		case "sabado":
+		case "domingo":
+		{
+			alert("es fin de semana");
+			break;
+		}
+
+		default:
+		{
+			alert("a trabajar!!");
+			break;
+		}
+	}
+*/
 
 
 
