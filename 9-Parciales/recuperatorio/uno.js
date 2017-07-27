fff9c74096e149177aa5a8c557f16fd554863092
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 13:14:29 -0300

    subo ejer 1

--- 9-Parciales/recuperatorio/uno.js
+++ 9-Parciales/recuperatorio/uno.js
@@ -1,5 +1,13 @@
 
 function Mostrar()
 {
+    var laBase;
+    var superficie;
+    
+    laBase=document.getElementById('laBase').value;
+    laBase=parseInt(laBase);
+    
+    superficie=laBase*laBase;
 
+    alert("la superficie es iguala: "+superficie);
 }
