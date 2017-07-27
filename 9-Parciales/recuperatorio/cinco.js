f8a9d407970e8928b9f699449475f51c16f7ca44
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 13:17:44 -0300

    subo ejer 5 recuperatorio 1

--- 9-Parciales/recuperatorio/cinco.js
+++ 9-Parciales/recuperatorio/cinco.js
@@ -1,4 +1,19 @@
 function Mostrar()
 {
+    var mes;
 
+    mes=prompt("ingresar mes del año");
+
+    switch(mes)
+    {
+        case "diciembre":
+            alert("se vienen las fiestas");
+            break;
+        case "enero":
+            alert("comienza el año");
+            break;
+         default:
+            alert("no es enero, ni diciembre");
+            break;
+    }
 }
