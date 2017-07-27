9835302569762d49ad37b0e4701dc15d759a732d
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 13:16:04 -0300

    subo ejer 3 recuperatorio 1

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,22 @@
 function Mostrar()
 {
+    var precioUno;
+    var precioDos;
+    var precioTres;
+    var suma;
+    var promedio;
 
+
+    precioUno=document.getElementById('precioUno').value;
+    precioUno= Number(precioUno);
+
+    precioDos=document.getElementById('precioDos').value;
+    precioDos= Number(precioDos);
+
+    precioTres=document.getElementById('precioTres').value;
+    precioTres= Number(precioTres);
+
+    suma= precioUno+precioDos+precioTres;
+    promedio=suma/3;
+    alert("la suma es: "+suma+"\nel promedio es: "+promedio);
 }
