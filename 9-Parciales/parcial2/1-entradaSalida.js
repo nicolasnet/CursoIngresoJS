86e55da77e7275e42ddf417bc4bbd5c321376be9
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 17:37:53 -0300

    ejer 1 parcial 2

--- 9-Parciales/parcial2/1-entradaSalida.js
+++ 9-Parciales/parcial2/1-entradaSalida.js
@@ -1,7 +1,17 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 1-EntradaSalida");
+	//alert("Funciona 1-EntradaSalida");
 	
+	var base;
+	var perimetro;
+
+
+	base=document.getElementById('lado').value;
+	base=Number(base);
+
+	perimetro=base*3;
+
+	alert("el perimetro es: "+perimetro);
 }
 
