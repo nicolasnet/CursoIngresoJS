f9c652e581e227201e3a75d0cfb233941b939416
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 20:39:19 -0300

    ejer 6 parcial 1
    finalizado este ejercicio, prolijo y funcionando

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,45 @@
 function Mostrar()
 {
+	var importe;
+	var contador=1;
+	var maximo;
+	var minimo;
+
+
+	while(contador<25)
+	{
+		
+		importe=prompt("ingresar el "+contador+"º importe");
+		importe=Number(importe);
+
+		while(importe<0)
+		{
+			importe=prompt("por favor ingresar importe mayor a cero");
+			importe=Number(importe);
+		}
+
+		if(contador==1)
+		{
+			minimo=importe;
+			maximo=importe;
+		}
+		
+		if(importe<minimo)
+		{
+			minimo=importe;
+		}
+
+		if(importe>maximo)
+		{
+			maximo=importe;
+		}
+
+
+		contador++
+
+	}
+
+	alert("el importe mayor fue: "+maximo+"\nel importe menor fue: "+minimo);
+
 
 }
