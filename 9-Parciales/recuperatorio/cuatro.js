b4f2a106bab66f32f126c34fbb341dc4b4492ccc
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 13:17:01 -0300

    subo ejer 4 recuperatorio 1

--- 9-Parciales/recuperatorio/cuatro.js
+++ 9-Parciales/recuperatorio/cuatro.js
@@ -1,4 +1,30 @@
 function Mostrar()
 {
+    var numeroUno;
+    var numeroDos;
+    var resultado;
 
+    numeroUno=prompt("ingrese 1er numero");
+    numeroUno=parseInt(numeroUno);
+
+    numeroDos=prompt("ingrese 2do numero");
+    numeroDos=parseInt(numeroDos);
+
+    resultado=numeroUno+numeroDos;
+
+    if(resultado==0)
+    {
+        document.write("el resultado es cero");
+    }
+    else
+    {
+        if(resultado>0)
+        {
+            document.write("el resultado es positivo");
+        }
+        else
+        {
+            document.write("el resultado es negativo");
+        }
+    }
 }
