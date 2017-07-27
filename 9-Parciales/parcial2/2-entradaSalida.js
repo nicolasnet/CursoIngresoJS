4ad7bb068d1f8043e97288f6aa7281e9f828bb24
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 17:39:09 -0300

    ejer 2 parcial 2
    empece a usar Number() en lugar de parseInt()

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -1,7 +1,18 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
+	//alert("Funciona 2-EntradaSalida");
 	
+	var precio;
+	var importe;
+	var iva=1.21,
+
+	precio=prompt("ingrese el precio");
+	precio=Number(precio);
+
+	importe=precio*iva;
+
+	//alert("el importe final es: "+importe);
+	document.getElementById('importe').value=importe;	
 }
 
