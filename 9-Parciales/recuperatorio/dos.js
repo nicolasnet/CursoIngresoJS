3b0890593d2218bcda2f4bc0d84e53c849ed28da
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 13:15:18 -0300

    subo ejer 2 recuperatorio 1

--- 9-Parciales/recuperatorio/dos.js
+++ 9-Parciales/recuperatorio/dos.js
@@ -1,4 +1,13 @@
 function Mostrar()
 {
-  
+    var importe;
+    var descuento=0.75;
+    var importeFinal;
+
+    importe=prompt("ingrese importe");
+    importe=parseInt(importe);
+
+    importeFinal=importe*descuento;
+
+    document.getElementById('importeFinal').value=importeFinal;
 }
