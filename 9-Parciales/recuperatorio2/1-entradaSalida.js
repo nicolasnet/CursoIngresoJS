9f2ebd177a95b043528a5ce6c02813ba687ecbd2
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 17:55:17 -0300

    ejer 1 recuperatorio 2

--- 9-Parciales/recuperatorio2/1-entradaSalida.js
+++ 9-Parciales/recuperatorio2/1-entradaSalida.js
@@ -1,7 +1,16 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 1-EntradaSalida");
+	//alert("Funciona 1-EntradaSalida");
 	
+	var lado;
+	var perimetro;
+
+	lado=document.getElementById('lado').value;
+	lado=Number(lado);
+
+	perimetro=lado*3;
+
+	alert("el perimetro es: "+perimetro);	
 }
 
