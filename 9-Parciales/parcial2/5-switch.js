ec686a1e636e6f202f542153535d6a1a81edda7e
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 17:42:42 -0300

    Ejer 5 parcial 2

--- 9-Parciales/parcial2/5-switch.js
+++ 9-Parciales/parcial2/5-switch.js
@@ -1,7 +1,21 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 5-Switch");
+	//alert("Funciona 5-Switch");
 	
+	var mes;
+	
+	mes=prompt("ingresar mes del año");
+
+	switch(mes)
+	{
+		case "enero":
+		case "febrero":
+			alert("veranito!!!!");
+			break;
+		default:
+			alert("extraño enero y febrero");
+			break;
+	}	
 }
 
