d91cfc55d47bff8a1e9c6040b4e6add92d28142f
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 17:41:40 -0300

    Ejer 4 parcial 2
    en este caso en lugar de hacer una variable para cada resultado, en el mismo alert hice el calculo. Importante poner el calculo entre paréntesis

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,31 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
+	//alert("Funciona 4-if");
 	
+	var numeroUno;
+	var numeroDos;
+
+	numeroUno=prompt("ingresar 1er numero");
+	numeroUno=Number(numeroUno);
+
+	numeroDos=prompt("ingresar 2do numero");
+	numeroDos=Number(numeroDos);
+
+	if(numeroUno==numeroDos)
+	{
+		document.write("son iguales, multiplicacion= "+(numeroUno*numeroDos));
+	}
+	else
+	{
+		if(numeroUno>numeroDos)
+		{
+			document.write("el 1ro es mayor, resta= "+(numeroUno-numeroDos));
+		}
+		else
+		{
+			document.write("el 2do es mayor, suma= "+(numeroUno+numeroDos));
+		}
+	}
 }
 
