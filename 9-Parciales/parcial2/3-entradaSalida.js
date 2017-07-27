7c8ab7a0c43968242737edf86a97be4c65ef962d
nicolasnet <niconetgomez@hotmail.com>
2017-07-27 17:39:52 -0300

    ejer 3 parcial 2

--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,25 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
+	//alert("Funciona 3-EntradaSalida");
 	
+	
+	var ancho;
+	var largo;
+	var perimetro;
+	var alambreNecesario;
+
+
+	ancho=document.getElementById('ancho').value;
+	ancho=Number(ancho);
+
+	largo=document.getElementById('largo').value;
+	largo=Number(largo);
+
+	perimetro=2*(ancho+largo);
+
+	alambreNecesario=perimetro*6;
+
+	alert("el alambre necesario es: "+alambreNecesario+" metros");
 }
 
