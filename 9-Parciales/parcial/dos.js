e96dc4d68d55996ddfeb1c2399d70cf4125888e2
nicolasnet <niconetgomez@hotmail.com>
2017-07-28 22:01:54 -0300

    practicas para parcial
    un poco de todo para practicar muchos ejemplos.

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,5 +1,6 @@
 function Mostrar()
 {
+/*
   var importe;
   var importeFinal;
 
@@ -9,6 +10,102 @@ function Mostrar()
   importeFinal=importe*1.21;
 
   document.getElementById('importeFinal').value=importeFinal;
+*/
+
+//Se piden 10 numeros, informar el promedio.
+/*
+	var importe;
+	var promedio;
+	var contador=0;
+	var acumulador=0;
+	var cantidad=10;
+
+	while(contador<cantidad)
+	{
+		contador++;
+		importe=prompt("ingresar "+contador+"° importe");
+		importe=Number(importe);
+		acumulador=acumulador+importe;
+		
+		
+
+
+	}
+
+	promedio=acumulador/cantidad;
+
+
+	console.log("el promedio es: "+promedio);
+*/
+
+//se piden 10 notas validadas entre cero y diez, informar la peor nota.
+
+/*	
+	var nota;
+	var notaMenor;
+	var cantidad=10;
+	var contador=0;
+
+	while(contador<cantidad)
+		{
+			contador++
+			nota=prompt("ingresar "+contador+"° nota");
+			nota=Number(nota);
+			while(nota<0 || nota>10)
+			{
+				nota=prompt("por favor ingresar nota entre 0 y 10");
+				nota=Number(nota);
+			}
+
+
+			if(contador==1)
+			{
+				notaMenor=nota;
+			}
+			else
+			{
+				if(nota<notaMenor)
+				{
+					notaMenor=nota;
+				}
+			}
+		}
+
+
+	console.log("la nota menor es: "+notaMenor);
+*/
+
+//estamos haciendo aparecer un cuadrado.... es algo simple para demostrar q no hay q complicarse
+
+	var i;
+
+
+	for(i=0; i<10; i++)
+	{
+
+		if(i==0 || i==9)
+		{
+			document.write("**********<br>");	
+		}
+		else
+		{
+			document.write("*00000000*<br>");
+		}
+		
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
 
 
 
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,12 +1,206 @@
 
 function Mostrar()
 {
-	var laBase;
+
+
+	/*var laBase;
 	var perimetro;
 
 	laBase=document.getElementById('laBase').value;
 	laBase=parseInt(laBase);
 	perimetro=laBase*4;
 
-	alert("el perimetro es: "+perimetro);
+	alert("el perimetro es: "+perimetro);*/
+
+
+
+//-----------------------------------Practicas para el parcial----------------------------------------------------------------------
+
+
+/*
+//se pide numero por ID y se muestra por alert ese numero al cubo
+
+	var laBase;
+	var cubo;
+
+	laBase=document.getElementById('laBase').value;
+	laBase=Number(laBase);
+
+	cubo=Math.pow(laBase, 3); //formula para potencias.
+	//cubo=laBase*laBase*laBase;
+
+
+	alert("el cubo del numero es: "+cubo);
+*/
+
+/*
+
+
+
+//Se piden dos numeros por prompt y mostramos en el cuadro de texto la suma de los  mismos.
+
+	var numeroUno;
+	var numeroDos;
+	var suma;
+
+	numeroUno=prompt("ingrese 1er numero");
+	numeroUno=Number(numeroUno);
+
+	numeroDos=prompt("ingrese 2do numero");
+	numeroDos=Number(numeroDos);
+
+	suma=numeroUno+numeroDos;
+
+	document.getElementById('laBase').value=suma;
+*/
+
+
+
+//Pedir por prompt nombre y nacionalidad y mostrar el mensaje en el cuadro de texto "Pepe es Argentino".
+/*	
+	var nombre;
+	var nacionalidad;
+
+	nombre=prompt("ingresar nombre");
+
+	nacionalidad=prompt("ingresar nacionalidad");
+
+
+
+
+	document.getElementById('laBase').value=nombre+" es "+nacionalidad;
+*/
+
+
+/*
+//pedir dos numeros y mostrar el menor por ID
+
+	var numeroUno;
+	var numeroDos;
+
+	numeroUno=prompt("ingresar numero uno");
+	numeroUno=Number(numeroUno);
+
+	numeroDos=prompt("ingresar numero dos");
+	numeroDos=Number(numeroDos);
+
+
+	if(numeroUno>numeroDos)
+	{
+		document.getElementById('laBase').value=numeroDos;
+	}
+	else
+	{
+		document.getElementById('laBase').value=numeroUno;
+	}
+*/	
+
+//Se pide nombre y edad y se informa por consola el nombre del mayor.
+/*
+	var nombreUno;
+	var nombreDos;
+	var edadUno;
+	var edadDos;
+	var NombreEdadMayor;
+
+	nombreUno=prompt("ingrese 1er nombre");
+	edad=prompt("ingrese 1er edad");
+
+	nombreDos=prompt("ingrese 2do nombre");
+	edadDos=prompt("ingrese 2da edad");
+
+	if(edadUno>edadDos)
+	{
+		NombreEdadMayor=nombreUno;
+	}
+	else
+	{
+		NombreEdadMayor=nombreDos;
+	}
+
+
+	console.log("el de mayor edad es: "+NombreEdadMayor);
+*/
+
+//Se ingresa por el cuadro de texto el importe a pagar de estacionamiento al precionar el boton nos mostrara, los sig mensajes:
+// 1- "baratito" si es menos de 20. 2- "esta bien" si es menor a 70. 3- "esto es un robo" si sale mas de 70.
+	
+/*
+	var importe;
+	var resultado;
+
+	importe=document.getElementById('laBase').value;
+
+	if(importe<20) 
+	{
+		resultado="baratito";
+	}
+	else
+	{
+		if(importe<70) 
+		{
+			resultado="esta bien";
+		}
+		else
+		{
+			resultado="es un robo";
+		}	
+	}
+
+	console.log(resultado);
+*/
+
+//se piden 3 numeros por prompt y se informa cual es el del medio.
+
+	var numeroUno;
+	var numeroDos;
+	var numeroTres;
+	var resultado;
+
+
+	numeroUno=prompt("ingresar 1er importe");
+	numeroDos=prompt("ingresar 2do importe");
+	numeroTres=prompt("ingresar 3er importe");
+
+	if((numeroUno>=numeroDos && numeroUno<=numeroTres) || (numeroUno<=numeroDos && numeroUno>=numeroTres) )
+	{
+		resultado=numeroUno;
+	}
+	else
+	{
+		if((numeroDos>=numeroUno && numeroDos<=numeroTres) || (numeroDos<=numeroUno && numeroDos>=numeroTres))
+		{
+			resultado=numeroDos;
+		}
+		else
+		{
+			resultado=numeroTres;
+		}
+
+
+		
+	}
+
+	console.log(resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
