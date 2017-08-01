205741a7412fa058c2f53a0f75d166ca1a651289
nicolasnet <niconetgomez@hotmail.com>
2017-07-31 23:04:42 -0300

    ej 7 y 8 parcial 1
    ejercicios terminados, solo habría q cambiar la cantidad de datos q se desea ingresar según ejercicio

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -1,4 +1,80 @@
 function Mostrar()
 {
+	var numeroPositivo;
+	var respuesta="si";
+	var contador=0;
+	var acumuladorNumeros=0;
+	var cantidadNumerosPares=0;
+	var promedio;
+	var maximo;
+	var minimo;
 
+
+	while(respuesta!="no")
+	{
+		contador++;
+
+		numeroPositivo=prompt("ingresar numero positivo nro: "+contador+" º");
+		//numeroPositivo=parseInt(numeroPositivo);
+		while(numeroPositivo<0)
+		{
+			numeroPositivo=prompt("ingresar numeros unicamente positivos");
+			//numeroPositivo=parseInt(numeroPositivo);
+		}
+		numeroPositivo=Number(numeroPositivo);
+
+
+			//numeros pares
+		if(numeroPositivo%2==0)	
+		{
+			cantidadNumerosPares++;
+		}
+
+
+			//acumulador para sacar promedios y suma total
+		acumuladorNumeros=acumuladorNumeros+numeroPositivo;
+
+
+			//maximo y minimo
+		if(contador==1)
+		{
+			minimo=numeroPositivo;
+			maximo=numeroPositivo;
+		}
+		else
+		{
+			if(numeroPositivo<minimo)
+			{
+				minimo=numeroPositivo;
+			}
+
+
+			if(numeroPositivo>maximo)
+			{
+				maximo=numeroPositivo;
+			}
+		}
+
+
+
+
+
+
+
+
+
+		respuesta=prompt("si no desea seguir, escribir `no´");
+	}
+
+
+
+	document.write("la cantidad de numeros pares es: "+cantidadNumerosPares);
+
+	promedio=acumuladorNumeros/contador;
+	document.write("</br>el promedio es: "+promedio);
+
+	document.write("</br>suma total de numeros= "+acumuladorNumeros);
+
+	document.write("</br>el numero maximo es: "+maximo);
+	document.write("</br>el numero minimo es: "+minimo);
 }
--- 9-Parciales/parcial/siete.js
+++ 9-Parciales/parcial/siete.js
@@ -1,4 +1,61 @@
 function Mostrar()
 {
+	var nota;
+	var sexo;
+	var contador=0;
+	var cantidadDeNotas=5; //cambiar por 100 para el ejercicio.
+	var acumuladorNotas=0;
+	var promedio;
+	var notaMin;
+	//var contadorMasculino=0;
+	var hombresNotaMayorIgual6=0;
 
+
+	while(contador<cantidadDeNotas)
+		{
+			contador++
+
+			nota=prompt("ingresar nota nro "+contador+"º");
+			while(nota<0 || nota>10)
+			{
+				nota=prompt("ingresar nota entre 0 y 10");
+			}
+			nota=Number(nota);
+
+
+			sexo=prompt("ingresar sexo f o m");
+			while(sexo!="f" && sexo!="m")
+			{
+				sexo=prompt("ingresar unicamente f o m");
+			}
+
+
+			acumuladorNotas=acumuladorNotas+nota;
+
+
+			if(contador==1)
+			{
+				notaMin=nota;
+			}
+			else
+			{
+				if (nota<notaMin) 
+				{
+					notaMin=nota;
+				}
+			}
+
+
+			if(sexo=="m" && nota>5)
+			{
+				hombresNotaMayorIgual6=hombresNotaMayorIgual6+1;
+			}				
+		}
+
+		promedio=acumuladorNotas/contador;
+		console.log("el promedio de notas es: "+promedio);
+
+		console.log("la nota minima fue: "+notaMin);
+		
+		console.log("cantidad de Hombres con nota igual o mayor a 6: "+hombresNotaMayorIgual6);
 }
